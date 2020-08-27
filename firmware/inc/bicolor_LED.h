49309653eab8c00b112ef8a61fc9693947c6a62d
jordan aceto <jordanaceto@gmail.com>
2020-08-27 11:46:36 -0400

    add bi-color LED software module

---
+++ firmware/inc/bicolor_LED.h
@@ -0,0 +1,65 @@
+/*
+--|----------------------------------------------------------------------------|
+--| FILE DESCRIPTION:
+--|   bicolor_LED.h provides an interface for the bicolor LED array used to
+--|   display the active ADSR.
+--|
+--|----------------------------------------------------------------------------|
+--| REFERENCES:
+--|   WP115WEGW_bicolor_LED datasheet
+--|
+--|----------------------------------------------------------------------------|
+*/
+
+#ifndef BICOLOR_LED_H_INCLUDED
+#define BICOLOR_LED_H_INCLUDED
+
+/*
+--|----------------------------------------------------------------------------|
+--| INCLUDE FILES
+--|----------------------------------------------------------------------------|
+*/
+
+#include "stm32f4xx.h"
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC DEFINES
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--| NAME: NUM_BICOLOR_LEDS
+--| DESCRIPTION: the number of bi-color LEDs
+--| TYPE: uint
+*/
+#define NUM_BICOLOR_LEDS (4u)
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC FUNCTION PROTOTYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/*------------------------------------------------------------------------------
+Function Name:
+    bicolor_LED_show_active_ADSR
+
+Function Description:
+    Update the bi-color LEDs to show the active adsr
+
+Parameters:
+    active_adsr: the active adsr.
+    is_master: boolean signal indicating if the active adsr is acting as master
+
+Returns:
+    None.
+
+Assumptions/Limitations:
+    Assumes that the active adsr is in the range [0, 3]
+
+    Assumes that all system initialization is complete.
+------------------------------------------------------------------------------*/
+void bicolor_LED_show_active_ADSR(uint32_t active_adsr, uint32_t is_master);
+
+#endif
---
+++ firmware/src/bicolor_LED.c
@@ -0,0 +1,42 @@
+/*
+--|----------------------------------------------------------------------------|
+--| FILE DESCRIPTION:
+--|   bicolor_LED.h provides the implementation for the bi-color LED array.
+--|
+--|----------------------------------------------------------------------------|
+--| REFERENCES:
+--|   WP115WEGW_bicolor_LED datasheet
+--|
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--|----------------------------------------------------------------------------|
+--| INCLUDE FILES
+--|----------------------------------------------------------------------------|
+*/
+
+#include "GPIO.h"
+#include "SPI3.h"
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC FUNCTION DEFINITIONS
+--|----------------------------------------------------------------------------|
+*/
+
+void bicolor_LED_show_active_ADSR(uint32_t active_adsr, uint32_t is_master)
+{
+    // set the red LED high if we're in master mode, else green
+    const uint8_t bit_pattern_to_write = is_master ? 1 << (active_adsr << 1) : 2 << (active_adsr << 1);
+
+    SPI3_Transmit(SPI3_RG_LEDS_CS_GPIO_Port, SPI3_RG_LEDS_CS_Pin, bit_pattern_to_write);
+}
+
+/*
+--|----------------------------------------------------------------------------|
+--| PRIVATE HELPER FUNCTION DEFINITIONS
+--|----------------------------------------------------------------------------|
+*/
+
+/* None */
