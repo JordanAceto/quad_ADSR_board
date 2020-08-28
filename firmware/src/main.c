65842fe7fce9727a4157355bdf17782a5bfd005c
jordan aceto <jordanaceto@gmail.com>
2020-08-28 09:01:57 -0400

    update main to demo basic features

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,6 +1,7 @@
 
 
 #include "Discrete_Input.h"
+#include "GPIO.h"
 #include "MCP4822.h"
 #include "SPI3.h"
 #include "stm32f4xx.h"
@@ -12,11 +13,11 @@ int main(void)
 
     while (1)
     {
-
+        // right now just demo a few features so I can verify an upload
         delay(5000);
-        GPIOC->ODR ^= GPIO_ODR_OD3;
+        STATUS_LED_GPIO_Port->ODR ^= (1u << STATUS_LED_Pin);
 
-        MCP4822_Write(GPIOB, 5u, MCP4822_CHANNEL_A, MCP4822_GAIN_1x, 0x0ABCu);
+        MCP4822_Write(SPI3_DAC2_CS_GPIO_Port, SPI3_DAC2_CS_Pin, MCP4822_CHANNEL_A, MCP4822_GAIN_1x, 0x0ABCu);
     }
 
     return 0;
