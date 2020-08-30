ed02bee378990c4fabd414f3963ec82aaee1e2e8
jordan aceto <jordanaceto@gmail.com>
2020-08-30 17:54:07 -0400

    fix bicolor LED bug

--- firmware/src/bicolor_LED.c
+++ firmware/src/bicolor_LED.c
@@ -28,7 +28,7 @@
 void bicolor_LED_show_active_ADSR(uint32_t active_adsr, uint32_t is_master)
 {
     // set the red LED high if we're in master mode, else green
-    const uint8_t bit_pattern_to_write = is_master ? 1 << (active_adsr << 1) : 2 << (active_adsr << 1);
+    const uint8_t bit_pattern_to_write = is_master ? 1u << (6u - (active_adsr << 1)) : 2u << (6u - (active_adsr << 1u));
 
     SPI3_Transmit(SPI3_RG_LEDS_CS_GPIO_Port, SPI3_RG_LEDS_CS_Pin, bit_pattern_to_write);
 }
